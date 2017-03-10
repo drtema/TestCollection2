f6e0aa0038bb1b24ce9d5ac12eee176b083fd173
drtema <dr.tema@gmail.com>
2017-03-10 15:37:21 +0200

    Added LinkedList object and project compiled

---
+++ src/com/brainacad/oop/testcollection2/Main.java
@@ -0,0 +1,23 @@
+package com.brainacad.oop.testcollection2;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Dr.tema on 10.03.17.
+ */
+public class Main {
+    public static void main(String... a){
+        List<String> arrayList = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add((int)(Math.random()*10),"number_" + i);
+        }
+
+        Iterator<String> iterator = arrayList.iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}
